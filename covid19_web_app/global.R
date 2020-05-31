438aecf549c6452659164d9ceeb8bed0ce2d274d
stufima <marcus.finck@student.fh-kiel.de>
2020-05-31 10:18:19 +0200

    move ui panels to global.r

--- covid19_web_app/global.R
+++ covid19_web_app/global.R
@@ -216,3 +216,48 @@ plotting <- function(regionchoice, plotchoice, daterange, switch_absolut_relativ
   }
   return(ggplotly(plot))
 }
+
+# UI
+#----
+getUIWorldInputPanel <- function(){
+  l_World_Input <- sidebarPanel(
+  selectizeInput(inputId = "regionchoice"
+                 , label = "Select up to 2 regions of interest:"
+                 , choices = regionlist, selected = c("Germany")
+                 , options = list(maxItems = 2)
+  )
+  , checkboxGroupInput(inputId = "plotchoice_values"
+                       , label = "Select values:"
+                       , choices = plotlist[1:4]
+                       , selected = c("net_infected")
+                       , inline = FALSE
+  )
+  , checkboxGroupInput(inputId = "plotchoice_trend"
+                       , label = "Select trend values:"
+                       , choices = plotlist[5:7]
+                       , inline = FALSE
+  )
+  , sliderInput(inputId = "daterange"
+                , label = "What Period are you interested in?"
+                , min = min_date
+                , max = max_date
+                , value = c(min(covid$date), max(covid$date))
+                , timeFormat="%Y-%m-%d"
+  )
+  , radioButtons(inputId = "switch_absolut_relative"
+                 , label ="Type of display:"
+                 , choices = c("Absolut", "Relative")
+                 , selected = "Absolut"
+                 , inline = TRUE
+  )
+  )
+  return (l_World_Input)
+}
+
+getUIWorldOutputPanel <- function() {
+  l_World_Output <- mainPanel(
+    plotlyOutput("plot1")
+    , plotlyOutput("plot2")
+  )
+  return(l_World_Output)
+}
